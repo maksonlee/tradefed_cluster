0babb0910611c29b3ff8625fcf7928077c5b7d0f
Jeffrey Lu <jeffreylu@google.com>
2021-03-30 11:05:50 -0700

    Internal change
    PiperOrigin-RevId: 365851374
    Change-Id: Ief9791540515c96bba726342fe48eda51a0f359f

--- tradefed_cluster/env_config.py
+++ tradefed_cluster/env_config.py
@@ -51,7 +51,6 @@ class EnvConfig(env_util.EnvConfig):
     self.plugin = registry.GetNoOpPlugin()
 
     # Service providers
-    self.acl_plugin = None
     self.app_manager = None
     self.file_storage = None
     self.mailer = None
